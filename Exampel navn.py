d641052027843d444641665cd9f2d307f704a500
Sina Parsa <Sina.P64@Outlook.com>
2025-05-12 22:41:01 +0330

    Add files via upload

---
+++ Exampel navn.py
@@ -0,0 +1,10 @@
+##Fungerer i forhold til alfabetisk rækkefølge stor bogstaver har første priotitet og små bogstaver har mindste.
+
+name = ("Sina" , "Lars" , "Jesper" , "Chris" , "Christina")
+
+Min = min(name)
+
+Max = max(name)
+
+print(Min)
+print(Max)
---
+++ Exampel tal.py
@@ -0,0 +1,6 @@
+##Bruges til find det mindste og største 
+age = (49 , 35 , 49 , 55 , 37 , 45)
+
+min_max = min(age) , max(age)
+
+print(min_max)
